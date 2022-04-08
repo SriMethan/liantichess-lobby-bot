2b8c91b59f2b9790608811c626942661a35fe3e5
TheYoBots <yohaan.nathanjw@gmail.com>
2022-04-08 17:57:07 +0000

    Liantichess Support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# pychess-lobby-bot
+# liantichess-lobby-bot
 
-A discord bot that connects [Pychess](https://www.pychess.org) to [discord](https://discord.gg/aPs8RKr) using [discord.py](https://github.com/Rapptz/discord.py). The Bot is hosted on [Heroku's](https://heroku.com/) worker dynos. Check ['Environments'](https://github.com/gbtami/pychess-lobby-bot/deployments) to view hosted app (though only the app name can be seen as it runs on worker dynos not web dynos).
+A discord bot that connects [Liantichess](https://liantichess.herokuapp.com) to [discord](https://discord.gg/5qvjPQstKS) using [discord.py](https://github.com/Rapptz/discord.py). The Bot is hosted on [Heroku's](https://heroku.com/) worker dynos. Check ['Environments'](https://github.com/TheYobots/liantichess-lobby-bot/deployments) to view hosted app (though only the app name can be seen as it runs on worker dynos not web dynos).
 
 ## Local Installation
 ### Setup
@@ -13,11 +13,11 @@ pip install -r requirements.txt
 
 # Linux
 export DISCORD_TOKEN='xxxxxxxxx' # Add your discord token in place of xxxxxxxxx. The quotes must be kept.
-export PYCHESS=https://www.pychess.org # url to which your bot must communicate. Don't add a '/' at the end of the url.
+export LIANTICHESS=https://liantichess.herokuapp.com # url to which your bot must communicate. Don't add a '/' at the end of the url.
 
 # Windows
 setx DISCORD_TOKEN 'xxxxxxxxx' # Add your discord token in place of xxxxxxxxx. The quotes must be kept.
-setx PYCHESS https://www.pychess.org # url to which your bot must communicate. Don't add a '/' at the end of the url.
+setx LIANTICHESS https://liantichess.herokuapp.com # url to which your bot must communicate. Don't add a '/' at the end of the url.
 ```
 
 ### Run
@@ -30,9 +30,12 @@ python3 bot.py --help
 
 ## Heroku Installation
 ### Setup
-Create a Heroku app (both app name and region do not really matter). Then go to the `Deploys` tab in Heroku UI and select `Connect to GitHub` and click to search and select your fork of pychess-lobby-bot. Once that is done click on `deploy` (you could also select automatic deploy if you want heroku to automatically publish changes to your Dev instance).
+Create a Heroku app (both app name and region do not really matter). Then go to the `Deploys` tab in Heroku UI and select `Connect to GitHub` and click to search and select your fork of liantichess-lobby-bot. Once that is done click on `deploy` (you could also select automatic deploy if you want heroku to automatically publish changes to your Dev instance).
 
-In Heroku UI set config var `DISCORD_TOKEN` to the Token of your Discord Bot (which you can get from [here](https://discord.com/developers/applications)) and set `PYCHESS` to the url to which your bot must communicate. Don't add a '/' at the end of the url (example, `https://www.pychess.org`).
+In Heroku UI set config var `DISCORD_TOKEN` to the Token of your Discord Bot (which you can get from [here](https://discord.com/developers/applications)) and set `LIANTICHESS` to the url to which your bot must communicate. Don't add a '/' at the end of the url (example, `https://liantichess.herokuapp.com`).
 
 ### Run
 To start your heroku instance go to the `Resources` tab and enable the worker dyno. This will automatically start your discord bot.
+
+## Acknowlwdgements
+This code is a fork of [pychess-lobby-bot](https://github.com/gbtami/pychess-lobby-bot) and the code is completely by [@gbtami](https://github.com/gbtami) for [pychess](https://www.pychess.org/). Only minor edits were made to support [liantichess](https://liantichess.herokuapp.com)
--- bot.py
+++ bot.py
@@ -10,46 +10,17 @@ from discord.ext.commands import Bot
 log = logging.getLogger('discord')
 log.setLevel(logging.INFO)
 
-PYCHESS = os.getenv("PYCHESS", "http://127.0.0.1:8080")
+LIANTICHESS = os.getenv("LIANTICHESS", "http://127.0.0.1:8080")
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-SERVER_ID = 634298688663191582
-PYCHESS_LOBBY_CHANNEL_ID = 653203449927827456
-GAME_SEEK_CHANNEL_ID = 823862902648995910
-TOURNAMENT_CHANNEL_ID = 861234739820888074
-ANNOUNCEMENT_CHANNEL_ID = 865964574507008000
+SERVER_ID = 920263455779454997
+LIANTICHESS_LOBBY_CHANNEL_ID = 923820641386369084
+GAME_SEEK_CHANNEL_ID = 933405389133873173
+TOURNAMENT_CHANNEL_ID = 922504976775938088
+ANNOUNCEMENT_CHANNEL_ID = 920969508389257226
 
 ROLES = {
-    "gladiator": 867894147900637215,
-    "crazyhouse": 658544490830757919,
-    "capablanca": 658544637467951124,
-    "grand": 658544867269541899,
-    "atomic": 867889563087274034,
-    "hoppelpoppel": 867889843357876254,
-    "xiangqi": 658544904011644938,
-    "shogi": 658544950677340161,
-    "shogun": 675143932912599041,
-    "seirawan": 658753848982110209,
-    "shako": 658544983623860235,
-    "janggi": 695975424433455145,
-    "makruk": 658545040234119178,
-    "sittuyin": 658545093011046420,
-    "orda": 702977517018939444,
-    "synochess": 730903272080277524,
-    "shinobi": 867889352704131132,
-    "empire": 867892839493009478,
-    "chess": 658545185571209221,
-    "chak": 940232991182041098,
-    "chennis": 940233624048009236,
-}
-
-CATEGORIES = {
-    "chess": ("chess", "chess960", "crazyhouse", "crazyhouse960", "placement", "atomic", "atomic960"),
-    "fairy": ("capablanca", "capablanca960", "capahouse", "capahouse960", "seirawan", "seirawan960", "shouse", "grand", "grandhouse", "shako", "shogun", "hoppelpoppel"),
-    "army": ("orda", "synochess", "shinobi", "empire", "ordamirror", "chak", "chennis"),
-    "makruk": ("makruk", "makpong", "cambodian", "sittuyin", "asean"),
-    "shogi": ("shogi", "minishogi", "kyotoshogi", "dobutsu", "gorogoroplus", "torishogi"),
-    "xiangqi": ("xiangqi", "manchu", "janggi", "minixiangqi"),
+    "tournament": 964067396249866240,
 }
 
 intents = discord.Intents(messages=True, guilds=True)
@@ -59,7 +30,7 @@ class MyBot(Bot):
 
     async def on_message(self, msg):
         log.debug("---on_message() %s", msg)
-        if msg.author.id == self.user.id or msg.channel.id != PYCHESS_LOBBY_CHANNEL_ID:
+        if msg.author.id == self.user.id or msg.channel.id != LIANTICHESS_LOBBY_CHANNEL_ID:
             log.debug("---self.user msg OR other channel.id -> return")
             return
 
@@ -75,70 +46,16 @@ bot = MyBot(command_prefix="!", intents=intents)
 
 def get_role_mentions(bot, message):
     guild = bot.get_guild(SERVER_ID)
-    gladiator_role = guild.get_role(ROLES["gladiator"])
-    log.debug("guild, role, intent are: %s %s %s", guild, gladiator_role, gladiator_role.mention)
-
-    variant = message.split()[0].strip("*")
-
-    if variant in CATEGORIES["shogi"]:
-        role = guild.get_role(ROLES["shogi"])
-
-    elif variant in CATEGORIES["makruk"]:
-        role = guild.get_role(ROLES["makruk"])
-
-    elif variant == "janggi":
-        role = guild.get_role(ROLES["janggi"])
-
-    elif variant in CATEGORIES["xiangqi"]:
-        role = guild.get_role(ROLES["xiangqi"])
-
-    elif variant == "grand":
-        role = guild.get_role(ROLES["grand"])
-
-    elif variant == "shako":
-        role = guild.get_role(ROLES["shako"])
-
-    elif variant.startswith("atomic"):
-        role = guild.get_role(ROLES["atomic"])
-
-    elif variant.startswith("crazyhouse") or variant in ("shouse", "capahouse", "capahouse960", "grandhouse"):
-        role = guild.get_role(ROLES["crazyhouse"])
-
-    elif variant.startswith("capablanca"):
-        role = guild.get_role(ROLES["capablanca"])
-
-    elif variant.startswith("seirawan"):
-        role = guild.get_role(ROLES["seirawan"])
-
-    elif variant == "hoppelpoppel":
-        role = guild.get_role(ROLES["hoppelpoppel"])
-
-    elif variant == "shogun":
-        role = guild.get_role(ROLES["shogun"])
-    elif variant.startswith("orda"):
-        role = guild.get_role(ROLES["orda"])
-    elif variant == "synochess":
-        role = guild.get_role(ROLES["synochess"])
-    elif variant == "shinobi":
-        role = guild.get_role(ROLES["shinobi"])
-    elif variant == "empire":
-        role = guild.get_role(ROLES["empire"])
-    elif variant == "chak":
-        role = guild.get_role(ROLES["chak"])
-    elif variant == "chennis":
-        role = guild.get_role(ROLES["chennis"])
-    else:
-        role = guild.get_role(ROLES["chess"])
-
-    return "%s %s" % (gladiator_role.mention, role.mention)
-
+    tournament_role = guild.get_role(ROLES["tournament"])
+    log.debug("guild, role, intent are: %s %s %s", guild, tournament_role, tournament_role.mention)
+    return "%s" % (tournament_role.mention)
 
 async def lobby_task(bot):
     await bot.wait_until_ready()
 
-    # Get the pychess-lobby channel
-    pychess_lobby_channel = bot.get_channel(PYCHESS_LOBBY_CHANNEL_ID)
-    log.debug("pychess_lobby_channel is: %s", pychess_lobby_channel)
+    # Get the liantichess-lobby channel
+    liantichess_lobby_channel = bot.get_channel(LIANTICHESS_LOBBY_CHANNEL_ID)
+    log.debug("liantichess_lobby_channel is: %s", liantichess_lobby_channel)
 
     game_seek_channel = bot.get_channel(GAME_SEEK_CHANNEL_ID)
     log.debug("game_seek_channel is: %s", game_seek_channel)
@@ -153,7 +70,7 @@ async def lobby_task(bot):
         log.debug("+++ Creating new aiohttp.ClientSession()")
         session = aiohttp.ClientSession()
 
-        async with session.ws_connect(PYCHESS + '/wsl') as ws:
+        async with session.ws_connect(LIANTICHESS + '/wsl') as ws:
             bot.lobby_ws = ws
             await ws.send_json({"type": "lobby_user_connected", "username": "Discord-Relay"})
             async for msg in ws:
@@ -170,7 +87,7 @@ async def lobby_task(bot):
                                 await ws.send_json({"type": "pong"})
                             elif data['type'] == 'lobbychat' and data['user'] and data['user'] != "Discord-Relay":
                                 log.debug("+++ lobbychat msg: %s %s", data['user'], data["message"])
-                                await pychess_lobby_channel.send("**%s**: %s" % (data['user'], data['message']))
+                                await liantichess_lobby_channel.send("**%s**: %s" % (data['user'], data['message']))
                             elif data['type'] == 'create_seek':
                                 log.debug("+++ create_seek msg: %s", data["message"])
                                 await game_seek_channel.send("%s" % data['message'])
@@ -197,7 +114,7 @@ background_task = bot.loop.create_task(lobby_task(bot))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='PyChess-Variants discord bot')
+    parser = argparse.ArgumentParser(description='Liantichess discord bot')
     parser.add_argument('-v', action='store_true', help='Verbose output. Changes log level from INFO to DEBUG.')
     parser.add_argument('-w', action='store_true', help='Less verbose output. Changes log level from INFO to WARNING.')
     args = parser.parse_args()
